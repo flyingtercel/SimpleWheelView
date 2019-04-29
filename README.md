188565255c2ba6addfa374962ebd3c31bffa025d
wdx <flyingtercel@163.com>
2019-04-29 15:33:01 +0800

    设置滚动监听事件

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # SimpleWheelView
 继承View实现简单的滚轮控件
+![image]()
+
+### 1添加依赖
+```
+    allprojects {
+		repositories {
+			...
+			maven { url 'https://jitpack.io' }
+		}
+	}
+	dependencies {
+    	implementation 'com.github.flyingtercel:SimpleWheelView:Tag'
+    }
+```
+### 2在布局中引用
+```
+    <com.mifeng.us.widget.WheelView
+        android:id="@+id/mWheelView"
+        android:layout_width="match_parent"
+        android:layout_marginTop="35dp"
+        android:layout_height="235dp"
+        app:normalTextColor="#999999"
+        app:normalTextSize="14sp"
+        app:selectedTextColor="#333333"
+        app:selectedTextSize="16sp"
+        app:itemNumber="7"
+        app:lineHeight="1dp"
+        app:lineColor="#cc0000"
+        app:isCyclic="false"
+        app:noEmpty="false"/>
+```
+### 3设置滚动数据
+```
+    ArrayList<String> list = new ArrayList<>();
+    for (int i = 0; i < 15; i++) {
+        list.add("滑动数据第" + i + "个条目");
+    }
+    mWheelView.setData(list);
+
+```
+### 4设置滑动监听
+```
+    对滑动时间做监听
+    mWheelView.setOnSelectListener(new WheelView.OnSelectListener() {
+        @Override
+        public void endSelect(int id, String value) {
+            mTextView.setText(value);
+        }
+        @Override
+        public void selecting(int id, String value) {
+            mTextView.setText(value);
+        }
+    });
+```
--- app/src/main/java/com/mifeng/us/wheelview/MainActivity.java
+++ app/src/main/java/com/mifeng/us/wheelview/MainActivity.java
@@ -2,6 +2,7 @@ package com.mifeng.us.wheelview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.mifeng.us.widget.WheelView;
 
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private WheelView mWheelView;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +19,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mWheelView = findViewById(R.id.mWheelView);
+        mTextView = findViewById(R.id.mTextView);
 
         initDate();
     }
 
     private void initDate() {
-        ArrayList<String>list = new ArrayList<>();
-        for(int i=0;i<15;i++){
-            list.add("滑动数据第"+i+"个条目");
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            list.add("滑动数据第" + i + "个条目");
         }
         mWheelView.setData(list);
 
+        /*对滑动时间做监听*/
+        mWheelView.setOnSelectListener(new WheelView.OnSelectListener() {
+            @Override
+            public void endSelect(int id, String value) {
+                mTextView.setText(value);
+            }
+
+            @Override
+            public void selecting(int id, String value) {
+                mTextView.setText(value);
+            }
+        });
     }
 }
